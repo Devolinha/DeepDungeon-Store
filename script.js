cd396c8f30feb94d15cab87b6516721cbb37e7ff
Devolinha <joaomonteiro.art@gmail.com>
2022-01-05 16:10:53 -0300

    .

--- script.js
+++ script.js
@@ -5,10 +5,35 @@ var MenuItems =  document.getElementById("MenuItems");
 MenuItems.style.maxHeight = "0px";
 
 function menutoggle(){
-    if(MenuItems.style.maxHeight == "0px"){
-        MenuItems.style.maxHeight = "200px";
+    if(MenuItems.style.maxHeight == "0px")
+    {
+        MenuItems.style.maxHeight = "200px"
     }
-    else{
-        MenuItems.style.maxHeight = "0px";
+    else
+    {
+        MenuItems.style.maxHeight = "0px"
     }
+}
+
+//modal iniciar
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    if(modal){
+        modal.classList.add('show');
+        modal.addEventListener('click', (e) => {
+        if(e.target.id == modalID || e.target.className == 'button-close' ){
+            modal.classList.remove('show')
+        }
+    });
+    }
+    
+}
+
+const item = document.getElementsByClassName ('item-img');
+console.log(item)
+for (var i = 0; i< item.length; i++){
+item[i].addEventListener('click', function(){
+    console.log('inicia')
+    iniciaModal('item-modal');
+});
 }
